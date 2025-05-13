0390be3f81d092792784619ad880d0ecf2984eaa
Séra Balázs <serabali10050@gmail.com>
2025-05-13 20:30:00 +0200

    Update darklystyle.cpp

--- kstyle/darklystyle.cpp
+++ kstyle/darklystyle.cpp
@@ -3914,15 +3914,14 @@ bool Style::drawFrameLineEditPrimitive(const QStyleOption *option, QPainter *pai
         // Fix needed: if Dolphin location is Editable
 
         // render
-        if (_isDolphin && widget->inherits("DolphinUrlNavigator"))
-            {
-            const auto rect(option->rect.adjusted(0, -2, 0, 2));
-            _helper->renderLineEdit(painter, rect, background, outline, hasFocus, mouseOver, enabled, windowActive, mode, opacity);
+        if (isVisible) {
+            QLineEdit* dolphinLineEdit = widget->findChild<QLineEdit *>();
+            if (dolphinLineEdit->isVisible()) {
+                _helper->renderLineEdit(painter, rect.adjusted(0, -2, 0, 2), background, outline, hasFocus, mouseOver, enabled, windowActive, mode, opacity);
             }
-        else
-            {
+        } else {
             _helper->renderLineEdit(painter, rect, background, outline, hasFocus, mouseOver, enabled, windowActive, mode, opacity);
-            }
+        }
     }
 
     return true;
